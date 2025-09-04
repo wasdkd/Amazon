7aff9b87ef77de1895a034a9b91590c2d07c3b7a
gudeng <87080791+wasdkd@users.noreply.github.com>
2025-09-04 14:47:04 +0800

    Delete api directory

--- api/delete.js
+++
@@ -1,9 +0,0 @@
-
-window.deleteFile = async function (path, sha) {
-    const tok = prompt("请输入 GitHub Personal Access Token：");
-    if (!tok) return;
-    try {
-        await GH.callGitHub("DELETE", path, { message: `delete ${path}`, sha, branch: GH.BRANCH }, tok);
-        alert("删除成功！即将刷新"); location.reload();
-    } catch (err) { alert("失败：" + err); }
-};
